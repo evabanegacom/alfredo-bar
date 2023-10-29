8d0e61c23ba314fcb330a1979a585a6ee721ea7e
Precious Udegbue <Precious.Udegbue@sterling.ng>
2023-10-29 19:02:25 +0100

    new Last on Alfredo

--- src/App.js
+++ src/App.js
@@ -1,88 +1,39 @@
-// import React, { lazy, Suspense} from 'react';
-// import {Navbar} from './components';
+// import React from 'react';
+// import { Navbar } from './components';
 // import './App.css';
-
-// const AboutUs = lazy(() => import('./container/AboutUs/AboutUs'));
-// const Chef = lazy(() => import('./container/Chef/Chef'));
-// const FindUs = lazy(() => import('./container/Findus/FindUs'));
-// const Footer = lazy(() => import('./container/Footer/Footer'));
-// const Gallery = lazy(() => import('./container/Gallery/Gallery'));
-// const Header = lazy(() => import('./container/Header/Header'));
-// const Laurels = lazy(() => import('./container/Laurels/Laurels'));
-// const SpecialMenu = lazy(() => import('./container/Menu/SpecialMenu'));
+// import { AboutUs, Header, SpecialMenu, Chef, Laurels, Gallery, FindUs, Footer } from './container';
 
 // const App = () => (
 //   <div>
 //   <Navbar />
-//   <div><Suspense fallback={<div> Loading... </div>}></Suspense>< Header/> </div>
-//   <div><Suspense fallback={<div> Loading... </div>}></Suspense>< AboutUs/> </div>
-//   <div><Suspense fallback={<div> Loading... </div>}></Suspense>< SpecialMenu/> </div>
-//   <div><Suspense fallback={<div> Loading... </div>}></Suspense>< Chef/> </div>
-//   <div><Suspense fallback={<div> Loading... </div>}></Suspense>< Laurels/> </div>
-//   <div><Suspense fallback={<div> Loading... </div>}></Suspense>< Gallery/> </div>
-//   <div><Suspense fallback={<div> Loading... </div>}></Suspense>< FindUs/> </div>
-//   <div><Suspense fallback={<div> Loading... </div>}></Suspense>< Footer/> </div>
-//        </div>
-//         );
+//   <Header />
+//   <AboutUs />
+//   <SpecialMenu />
+//   <Chef />
+//   <Laurels />
+//   <Gallery />
+//   <FindUs />
+//   <Footer />
+//   </div >
+//       );
 
-//      export default App;
-
-import React, { lazy, Suspense } from 'react';
+// export default App;
+import React from 'react';
 import { Navbar } from './components';
 import './App.css';
-
-const AboutUs = lazy(() => import('./container/AboutUs/AboutUs'));
-const Chef = lazy(() => import('./container/Chef/Chef'));
-const FindUs = lazy(() => import('./container/Findus/FindUs'));
-const Footer = lazy(() => import('./container/Footer/Footer'));
-const Gallery = lazy(() => import('./container/Gallery/Gallery'));
-const Header = lazy(() => import('./container/Header/Header'));
-const Laurels = lazy(() => import('./container/Laurels/Laurels'));
-const SpecialMenu = lazy(() => import('./container/Menu/SpecialMenu'));
+import { AboutUs, Header, SpecialMenu, Chef, Laurels, Gallery, FindUs, Footer } from './container';
 
 const App = () => (
   <div>
     <Navbar />
-    <div>
-      <Suspense fallback={<div> Loading... </div>}>
-        <Header />
-      </Suspense>
-    </div>
-    <div>
-      <Suspense fallback={<div> Loading... </div>}>
-        <AboutUs />
-      </Suspense>
-    </div>
-    <div>
-      <Suspense fallback={<div> Loading... </div>}>
-        <SpecialMenu />
-      </Suspense>
-    </div>
-    <div>
-      <Suspense fallback={<div> Loading... </div>}>
-        <Chef />
-      </Suspense>
-    </div>
-    <div>
-      <Suspense fallback={<div> Loading... </div>}>
-        <Laurels />
-      </Suspense>
-    </div>
-    <div>
-      <Suspense fallback={<div> Loading... </div>}>
-        <Gallery />
-      </Suspense>
-    </div>
-    <div>
-      <Suspense fallback={<div> Loading... </div>}>
-        <FindUs />
-      </Suspense>
-    </div>
-    <div>
-      <Suspense fallback={<div> Loading... </div>}>
-        <Footer />
-      </Suspense>
-    </div>
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
   </div>
 );
 
